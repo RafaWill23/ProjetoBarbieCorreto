2096d078ef3a3721699841e12001927bed796438
Rafael Barbosa <141331069+RafaWill23@users.noreply.github.com>
2023-11-28 09:00:49 -0400

    Update Filme.tsx

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,10 +1,8 @@
-//Como importar uma imagem em REACT
-import Imagem from "./../../assets/barbie.png"
 import './Filme.css'
 type FilmeProps = {
     titulo:string,
-    sinopse:string,
-    imagem:string
+    descricao:string,
+    foto:string
 }
 function Filme(props:FilmeProps){
     return(
@@ -16,10 +14,10 @@ function Filme(props:FilmeProps){
             {/* Se não apareceu a imagem é provavel que você não tenha a imagem na pasta assets */}
             {/* Pode ser no componente, mas o caminho deve estar correto. */}
             {/* Imagem está no meu git (Não) */}
-            <img className="img-filme" src={Imagem} alt="" />
+            <img className="img-filme" src={props.foto} alt="" />
             <div className="text-barbie">
                 <h1>{props.titulo}</h1>
-                <p className="sinopse">{props.sinopse}</p>
+                <p className="sinopse">{props.descricao}</p>
             </div>
         </div>
     )
