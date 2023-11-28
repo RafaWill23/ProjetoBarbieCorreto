920e64b6b34243604aafe1fba895eaa89df3f5b8
Rafael Barbosa <141331069+RafaWill23@users.noreply.github.com>
2023-11-28 09:06:48 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import "./main.css"
+import{createBrowserRouter, RouterProvider} from "react-router-dom"
+import Header from './componentes/header/Header.tsx';
+import Main from './componentes/main/Main.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <> 
+    <Header/>
+    <Main/>
+    </>,
+  },
+  {
+    path: "/noticias",
+    element: <div>Pagina de noticias</div>,
+  },
+  {
+    path: "/sobre",
+    element: <div>Pagina Sobre</div>,
+  },
+  {
+    path: "/contato",
+    element: <div>Pagina de Contado</div>,
+  },
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router = {router}/>
+  </React.StrictMode>,
 )
